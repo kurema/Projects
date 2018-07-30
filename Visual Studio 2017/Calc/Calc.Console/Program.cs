6b2e06abede79da12a0520b076256780ecf4145a
kurema <kurema_makoto_software@yahoo.co.jp>
2018-07-30 22:57:48 +0900

    CalcApp
    CalcAppレポジトリを作ったので移す、というか作り直します。

---
+++ Visual Studio 2017/Calc/Calc.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Numerics;
+
+namespace Calc.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = System.Console.ReadLine();
+                if (input == "exit") return;
+
+                BigInteger a = new BigInteger(145);
+                System.Console.WriteLine(a * a);
+            }
+
+        }
+    }
+}
---
+++ Visual Studio 2017/Calc/Calc.Helper/ExpressionReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+using System.Numerics;
+
+namespace Calc.Helper
+{
+    public class ExpressionReader
+    {
+    }
+}
