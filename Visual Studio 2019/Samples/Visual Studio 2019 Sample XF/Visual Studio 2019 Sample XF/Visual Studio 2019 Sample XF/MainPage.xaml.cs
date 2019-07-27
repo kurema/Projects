a34b8a0a48a616a44c6350de78b2cc6468b49227
kurema <kurema_makoto_software@yahoo.co.jp>
2019-07-27 19:57:35 +0900

    Twitter and others

---
+++ Visual Studio 2019/Samples/Visual Studio 2019 Sample XF/Visual Studio 2019 Sample XF/Visual Studio 2019 Sample XF/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Visual_Studio_2019_Sample_XF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
