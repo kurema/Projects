e85fbae5a4630da89af9bd909d01ea3cdd6b8b8f
kurema <kurema_makoto_software@yahoo.co.jp>
2021-01-22 22:26:54 +0900

    https://www.youtube.com/watch?v=xS_AlnXMFwo

---
+++ Visual Studio 2019/Samples/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)(-1).ToString(new System.Globalization.CultureInfo("ar"))[0]);
+            Console.WriteLine((-1).ToString(new System.Globalization.CultureInfo("ar")));
+            Console.WriteLine((-1.2).ToString(new System.Globalization.CultureInfo("fr")));
+        }
+    }
+}
