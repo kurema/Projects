6ebecc4c43f77e601ea87041f172924b95e540df
kurema <kurema_makoto_software@yahoo.co.jp>
2022-11-16 17:11:46 +0900

    Update MainPage.xaml.cs

--- Visual Studio 2022/WebViewTestApp/WebViewTestApp/MainPage.xaml.cs
+++ Visual Studio 2022/WebViewTestApp/WebViewTestApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -32,6 +33,7 @@ namespace WebViewTestApp
 
         private void WebView_WebResourceRequested1(WebView sender, WebViewWebResourceRequestedEventArgs args)
         {
+            //System.Exception: 'アプリケーションは、別のスレッドにマーシャリングされたインターフェイスを呼び出しました。 (Exception from HRESULT: 0x8001010E (RPC_E_WRONG_THREAD))'
             var source = sender.Source; // Exception!
         }
 
@@ -69,12 +71,12 @@ namespace WebViewTestApp
         {
             var deferral = args.GetDeferral();
             var sb = new StringBuilder();
-            sb.AppendLine($"1: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            sb.AppendLine($"1: {Thread.CurrentThread.ManagedThreadId}");
             await sender.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                sb.AppendLine($"2: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                sb.AppendLine($"2: {Thread.CurrentThread.ManagedThreadId}");
             });
-            sb.AppendLine($"3: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            sb.AppendLine($"3: {Thread.CurrentThread.ManagedThreadId}");
             Debug.WriteLine(sb.ToString());
             //1: 7
             //2: 3
@@ -88,24 +90,24 @@ namespace WebViewTestApp
         {
             var deferral = args.GetDeferral();
             var sb = new StringBuilder();
-            sb.AppendLine($"1: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            sb.AppendLine($"1: {Thread.CurrentThread.ManagedThreadId}");
             await Task.Run(async () =>
             {
-                sb.AppendLine($"2: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                sb.AppendLine($"2: {Thread.CurrentThread.ManagedThreadId}");
                 await sender.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
                 {
-                    sb.AppendLine($"3: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                    sb.AppendLine($"3: {Thread.CurrentThread.ManagedThreadId}");
                 });
-                sb.AppendLine($"4: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+                sb.AppendLine($"4: {Thread.CurrentThread.ManagedThreadId}");
             }).ConfigureAwait(true);
-            sb.AppendLine($"5: {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+            sb.AppendLine($"5: {Thread.CurrentThread.ManagedThreadId}");
             Debug.WriteLine(sb.ToString());
             //1: 7
             //2: 5
             //3: 3
             //4: 5
             //5: 5
-            args.Response = new Windows.Web.Http.HttpResponseMessage(Windows.Web.Http.HttpStatusCode.Forbidden);
+            args.Response = new Windows.Web.Http.HttpResponseMessage(Windows.Web.Http.HttpStatusCode.Forbidden);//Exception!
             deferral.Complete();
             deferral.Dispose();
         }
